6bdc74b9cd78f876d1acb811439b07f7ca51d427
Lucas Ribeiro <lucas.daniel@gmail.com>
2019-06-12 20:08:10 -0400

    Creating Nav Items with Array and Map

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,13 +3,33 @@ import { Navbar, Row } from 'react-materialize';
 import { NavLink } from 'react-router-dom';
 
 const header = () => {
+    const sections = [{
+        name: "Home",
+        url: "/"
+    },
+    {
+        name: "About",
+        url: "about"
+    },
+    {
+        name: "Services",
+        url: "services"
+    },
+    {
+        name: "Contact",
+        url: "contact"
+    }];
+
+    const navLinks = sections.map(section => {
+        return (
+            <NavLink to={section.url}>{section.name}</NavLink>
+        )
+    });
+
     return (
         <Row>
             <Navbar className="blue-grey darken-4">
-                <NavLink to="/">Home</NavLink>
-                <NavLink to="about">About</NavLink>
-                <NavLink to="services">Services</NavLink>
-                <NavLink to="contact">Contact</NavLink>
+                {navLinks}
             </Navbar>
         </Row>
     );
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Header from '../components/Header/Header';
-import Main from './Main';
+// import Main from './Main';
 
 function App() {
   return (
