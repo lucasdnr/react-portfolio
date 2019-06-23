969025258a7498e0513a29cddf854e31e3179297
Lucas Ribeiro <lucas.daniel@gmail.com>
2019-06-22 23:39:17 -0400

    Developed About, Contact, Portfolio components

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,7 +1,80 @@
 import React from "react";
+import Cabin from '../../assets/img/portfolio/cabin.png';
+import Cake from '../../assets/img/portfolio/cake.png';
+import Circus from '../../assets/img/portfolio/circus.png';
+import Game from '../../assets/img/portfolio/game.png';
+import Safe from '../../assets/img/portfolio/safe.png';
+import Submarine from '../../assets/img/portfolio/submarine.png';
 
-const portfolio = () => (
-  
-);
+
+const portfolio = () => {
+    const portItems = [
+        {
+            target: '#portfolioModal1',
+            src: Cabin,
+        },
+        {
+            target: '#portfolioModal2',
+            src: Cake,
+        },
+        {
+            target: '#portfolioModal3',
+            src: Circus,
+        },
+        {
+            target: '#portfolioModal4',
+            src: Game,
+        },
+        {
+            target: '#portfolioModal5',
+            src: Safe,
+        },
+        {
+            target: '#portfolioModal6',
+            src: Submarine,
+        }
+    ];
+
+    const portLinks = portItems.map((section, index) => {
+        return (
+            <div className="col-md-6 col-lg-4" key={index}>
+                {/* Portfolio Item */ } 
+                <div className="portfolio-item mx-auto" data-toggle="modal" data-target={section.target}>
+                    <div className="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
+                        <div className="portfolio-item-caption-content text-center text-white">
+                            <i className="fas fa-plus fa-3x"></i>
+                        </div>
+                    </div>
+                    <img className="img-fluid" src={section.src} alt="" />
+                </div>
+            </div >
+        )
+    });
+
+    return (
+        <section className="page-section portfolio" id="portfolio">
+            <div className="container">
+
+                {/* Portfolio Section Heading */}
+                <h2 className="page-section-heading text-center text-uppercase text-secondary mb-0">Portfolio</h2>
+
+                {/* Icon Divider */}
+                <div className="divider-custom">
+                    <div className="divider-custom-line"></div>
+                    <div className="divider-custom-icon">
+                        <i className="fas fa-star"></i>
+                    </div>
+                    <div className="divider-custom-line"></div>
+                </div>
+
+                {/* Portfolio Grid Items */}
+                <div className="row">
+                    {portLinks}
+                </div>
+                {/*.row */}
+            </div>
+        </section>
+    );
+}
 
 export default portfolio;
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import Navigation from '../components/Navigation/Navigation';
 import Masthead from '../components/Masthead/Masthead';
+import Portfolio from '../components/Portfolio/Portfolio';
+import About from '../components/About/About';
+import Contact from '../components/Contact/Contact';
 
 
 function App() {
@@ -10,6 +13,12 @@ function App() {
       <Navigation />
       {/* Masthead */}
       <Masthead />
+      {/* Portfolio Grid Items */}
+      <Portfolio />
+      {/* About Section */}
+      <About />
+      {/* Contact Section */}
+      <Contact />
     </div>
   );
 }
