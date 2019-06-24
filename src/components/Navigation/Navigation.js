5f2bd8dc8ae17e9f8a434de87a1910f141c14b9a
Lucas Ribeiro <lucas.daniel@gmail.com>
2019-06-23 23:34:29 -0400

    Implemented React Scroll

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,42 +1,69 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import { Link, animateScroll as scroll } from "react-scroll";
 
-const navigation = () => {
-    const sections = [{
-        name: "Portfolio",
-        url: "#portfolio"
-    },
-    {
-        name: "About",
-        url: "#about"
-    },
-    {
-        name: "Contact",
-        url: "#contact"
-    }];
+const Navigation = () => {
+  const inputRef = useRef();
 
-    const navLinks = sections.map( (section, index)  => {
-        return (
-            <li className="nav-item mx-0 mx-lg-1" key={index}>
-                <a className="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href={section.url}>{section.name}</a>
-            </li>
-        )
-    });
+  const sections = [{
+    name: "Portfolio",
+    url: "portfolio"
+  },
+  {
+    name: "About",
+    url: "about"
+  },
+  {
+    name: "Contact",
+    url: "contact"
+  }];
 
+  const navLinks = sections.map((section, index) => {
     return (
-        <nav className="navbar navbar-expand-lg bg-secondary text-uppercase fixed-top" id="mainNav">
-        <div className="container">
-          <a className="navbar-brand js-scroll-trigger" href="#page-top">Start Bootstrap</a>
-          <button className="navbar-toggler navbar-toggler-right text-uppercase font-weight-bold bg-primary text-white rounded" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-            Menu
+      <li className="nav-item mx-0 mx-lg-1" key={index}>
+        <Link
+          activeClass="active"
+          to={section.url}
+          spy={true}
+          smooth={true}
+          offset={-70}
+          duration={500}
+          className="nav-link py-3 px-0 px-lg-3 rounded"
+          href="">
+          {section.name}
+        </Link>
+      </li>
+    )
+  });
+
+  const scrollToTop = () => {
+    scroll.scrollToTop();
+  };
+
+  const handleScroll = () => {
+    let offsetTop = inputRef.current.getBoundingClientRect().top;
+    console.log('Top ' + offsetTop);
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+  });
+
+  return (
+    <nav className="navbar navbar-expand-lg bg-secondary text-uppercase fixed-top" id="mainNav" ref={inputRef}>
+      <div className="container">
+        <a className="navbar-brand js-scroll-trigger" href="#page-top" onClick={scrollToTop}>Start Bootstrap</a>
+        <button className="navbar-toggler navbar-toggler-right text-uppercase font-weight-bold bg-primary text-white rounded" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          Menu
             <i className="fas fa-bars"></i>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarResponsive">
-            <ul className="navbar-nav ml-auto">
-             {navLinks}
-            </ul>
-          </div>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarResponsive">
+          <ul className="navbar-nav ml-auto">
+            {navLinks}
+          </ul>
         </div>
-      </nav>
-    );
-}
-export default navigation;
+      </div>
+    </nav>
+  );
+};
+
+export default Navigation;
--- src/containers/App.js
+++ src/containers/App.js
@@ -8,7 +8,7 @@ import Footer from '../components/Footer/Footer';
 
 
 function App() {
-  return (
+   return (
     <div>
       {/* Navigation */}
       <Navigation />
